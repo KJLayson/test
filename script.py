88e1682277cf168c488039fee7779f4863b5ccef
KJLayson <Laysonkian@live.com>
2025-02-04 17:42:29 -0800

    new function multiply added

--- script.py
+++ script.py
@@ -4,6 +4,10 @@ def greet(name):
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print(greet("Alice"))
     print("Addition:", add(2, 3))
+    print("Multiplication:", multiply(2, 3))
